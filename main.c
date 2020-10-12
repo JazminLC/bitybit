9b1003b8e472f0ef5f00467dced901567e8c0e1f
JazminLC <jlc1839@gmail.com>
2020-10-12 13:19:28 -0500

    int arg

--- main.c
+++ main.c
@@ -2,14 +2,26 @@
 #include <stdlib.h>
 
 int main(int argc, char const **argv){
-	char *mystring;
+	//char *mystring;
+	int n;
 	//printf("%s\n",argv[0]);
+
 	if (argc<2){
 		printf("Error: You need to pass an argument.\n");
 		return 0;
 	}
-	mystring=malloc(sizeof(argv[1]));
-	sprintf(mystring,"%s",argv[1]);
-	printf("%s\n",mystring);
+
+	//mystring=argv[1];
+	n=atoi(argv[1]);
+	//printf("%d\n",n);
+	//sprintf(mystring,"%s",argv[1]);
+	//printf("%s\n",mystring);
+	int *array = malloc(n * sizeof(int));
+
+	for (int i = 0; i < n; ++i){
+		array[i]=i+1;
+		printf("%d----%p\n", array[i],&array[i]);
+	}
+	free(array);
 	return 1;
 }
